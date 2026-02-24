93b625154807bcc73c6cf293f541ff053ed5074d
Cristobal Sendin <cristobal.sendin@ml6.eu>
2026-02-24 13:29:09 +0100

    Solution cleanup

--- my_agent/agent.py
+++ my_agent/agent.py
@@ -4,15 +4,13 @@ The `root_agent` is used to evaluate your agent's performance.
 """
 
 from google.adk.agents import llm_agent
-from my_agent.tools import web_search
-from my_agent.tools import calculator
-from my_agent.tools import pdf_extract
+from my_agent.tools import calculator, pdf_extract
 
 root_agent = llm_agent.Agent(
     model='gemini-2.5-flash-lite',
     name='agent',
     description="A helpful assistant that can answer questions.",
     instruction="You are a helpful assistant that answers questions directly and concisely.",
-    tools=[web_search, calculator, pdf_extract],
+    tools=[calculator, pdf_extract],
     sub_agents=[],
 )
